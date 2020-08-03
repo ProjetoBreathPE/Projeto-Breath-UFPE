fc98b20f2cff03001a9c882f657bfd5d719a26d0
igortasuke <67940335+igortasuke@users.noreply.github.com>
2020-08-03 14:49:31 -0300

    Implementação da leitura do sensor de pressão
    Checar conformidade e precisão devido à não linearidade.

--- Projeto Eletroeletrônico/arduino/src/main.cpp
+++ Projeto Eletroeletrônico/arduino/src/main.cpp
@@ -50,3 +50,16 @@ void loop() {
   checkCOmm();
   // put your main code here, to run repeatedly:
 }
+
+float pressureReader(){
+  int Vsensor = analogRead(pressureSensor);
+  float Pmin = -10.0;
+  float Pmax = 120.0;
+  float convertVsensor = 5/1023;
+  //Tensão a 120 cmH2O = 4.5V nominal (entre 4.4 e 4.6)
+  //Tensão a 0 cmH2O = 0.35V nominal (entre 0.30 e 0.40)
+  //Tensão a -10 cmH2O = 0.28V novinal (entre 0.23 e 0.33)
+  
+  float pressure = (Pmax - Pmin) / (4.5 - 0.28) * convertVsensor;
+  return pressure;
+}
