2da7c61937b59afa30ec366b4597b94846c0d459
ProjetoBreathPE <66397633+ProjetoBreathPE@users.noreply.github.com>
2020-09-22 14:28:53 -0300

    Correções

--- Projeto Eletroeletrônico/arduino/src/Constantes.h
+++ Projeto Eletroeletrônico/arduino/src/Constantes.h
@@ -24,8 +24,9 @@ const int BAG_CLEAR_TOL = 10;   // The tolerance (clicks) to consider clear of b
 #define SWITCH_POSITION 11      //Chave de fim de curso
 #define SWITCH_MODE 2           //Modo de ventilação
 
-#define POT_PIP A2               //Pressão inspiratória de pico
-#define POT_INSP_TIME A3         //tempo de inspiração
+
+#define POT_INSP_TIME A2         //tempo de inspiração
+#define POT_PIP A3               //Pressão inspiratória de pico
 #define POT_FREQUENCE A4         //frequência respiratória
 #define POT_SENSIBILITY A5       //sensibilidade no modo assistido
 
@@ -33,15 +34,15 @@ const int BAG_CLEAR_TOL = 10;   // The tolerance (clicks) to consider clear of b
 
 
 // Limites dos controles
-const int FREQ_MIN = 1;
-const int FREQ_MAX = 60;
-const int FREQ_RESOLUC = 1;
 const float TINSP_MIN = 0.3;
 const float TINSP_MAX = 6;
 const float TINSP_RESOLUC = 0.1;
-const int PIP_MIN = 5;
-const int PIP_MAX = 55; 
-const int PIP_RESOLUC = 0.2; 
+const float PIP_MIN = 5;
+const float PIP_MAX = 55; 
+const float PIP_RESOLUC = 0.2; 
+const float FREQ_MIN = 1;
+const float FREQ_MAX = 60;
+const float FREQ_RESOLUC = 1;
 const float SENS_MIN = 0.4;
 const float SENS_MAX = 2;
 const float SENS_RESOLUC = 0.2;
@@ -54,30 +55,3 @@ const int VOLTAGE_CONVERSOR = 5/1023; // conversão do sinal digital para valore
 //Tensão a 0 cmH2O = 0.35V nominal (entre 0.30 e 0.40)
 //Tensão a -10 cmH2O = 0.28V novinal (entre 0.23 e 0.33)
 const float PRESSURE_CONVERSOR = VOLTAGE_CONVERSOR * (120 + 10) / (4.5 - 0.28);
-
-// Safety settings
-const float MAX_PRESSURE = 40.0;        // Trigger high pressure alarm
-const float MIN_PLATEAU_PRESSURE = 5.0; // Trigger low pressure alarm
-const float MAX_RESIST_PRESSURE = 2.0;  // Trigger high-resistance notification
-const float MIN_TIDAL_PRESSURE = 5.0;   // Trigger no-tidal-pressure alarm
-const float VOLUME_ERROR_THRESH = 50.0; // Trigger incomplete breath alarm
-const int MAX_MOTOR_CURRENT = 1000;     // Trigger mechanical failure alarm. Units (10mA)
-const float TURNING_OFF_DURATION = 5.0; // Turning-off alarm is on for this duration (s)
-const float MECHANICAL_TIMEOUT = 1.0;   // Time to wait for the mechanical cycle to finish before alarming
-
-// PID values for auto-tuned for PG188
-const unsigned long QPPS = 2000;
-const float VKP = 6.38650;
-const float VKI = 0.0;
-const float VKD = 0.0;
-const float PKP = 70.0;
-const float PKI = 0.2;
-const float PKD = 200.0;
-const unsigned long KI_MAX = 10;
-const unsigned long DEADZONE = 0;
-const unsigned long MIN_POS = -100;
-const unsigned long MAX_POS = 700;
-const unsigned long VEL_MAX = 1800;     // Maximum velocity (clicks/s) to command
-const unsigned long ACC_MAX = 200000;   // Maximum acceleration (clicks/s^2) to command
-
-#endif
