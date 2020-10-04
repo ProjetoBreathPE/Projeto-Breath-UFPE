dabeac1494505b308f883a60d8b6b5e088c51522
Igor Morimoto <igortasuke@gmail.com>
2020-10-03 22:54:13 -0300

    Ajustes

--- Projeto Eletroeletrônico/Códigos Teste/Codigo 2 Ster Motor/StepMotor.h
+++ Projeto Eletroeletrônico/Códigos Teste/Codigo 2 Ster Motor/StepMotor.h
@@ -6,14 +6,14 @@ namespace StepMotor {
 //Controle do motor de passo
 /////////////////////////////////////////
 
-const short DIR=53;
-const short STEP=51;
-const short NSLEEP=49;
-const short NRESET=47;
-const short MS3=45;
-const short MS2=43;
-const short MS1=41;
-const short NENABLE=39;
+const short DRIVER_EN=4;
+const short DRIVER_DIR=5;
+const short DRIVER_PUL=6;
+//const short NSLEEP=49;
+//const short NRESET=47;
+//const short MS3=45;
+//const short MS2=43;
+//const short MS1=41;
 
 unsigned long t0=0;
 bool Mstep=0; //sinal para ser enviado para o drive
@@ -22,39 +22,39 @@ bool Mdir=0; //direção de rotação do motor
 int Nspeed=1;  //Para ser ajustável
 int counter=0; //Não utilizada diretamente
 
-unsigned long dt=1000; //Tempo em micro segundos
+unsigned long dt=500; //Tempo em micro segundos
 
 void initMotor()
 {
 //Controle motor de passo
-  pinMode(DIR,OUTPUT);
-  pinMode(STEP,OUTPUT);
-  pinMode(NSLEEP,OUTPUT);
-  pinMode(NRESET,OUTPUT);
-  pinMode(MS3,OUTPUT);
-  pinMode(MS2,OUTPUT);
-  pinMode(MS1,OUTPUT);
-  pinMode(NENABLE,OUTPUT);
+  pinMode(DRIVER_EN,OUTPUT);
+  pinMode(DRIVER_DIR,OUTPUT);
+  pinMode(DRIVER_PUL,OUTPUT);
+ // pinMode(NSLEEP,OUTPUT);
+  //pinMode(NRESET,OUTPUT);
+  //pinMode(MS3,OUTPUT);
+  //pinMode(MS2,OUTPUT);
+  //pinMode(MS1,OUTPUT);
   
-  digitalWrite(DIR,LOW);
-  digitalWrite(STEP,LOW);
-  digitalWrite(NSLEEP,LOW);
-  digitalWrite(NRESET,HIGH);
-  digitalWrite(MS3,LOW);
-  digitalWrite(MS2,LOW);
-  digitalWrite(MS1,LOW);
-  digitalWrite(NENABLE,LOW);
+  digitalWrite(DRIVER_EN,LOW);
+  digitalWrite(DRIVER_DIR,LOW);
+  digitalWrite(DRIVER_PUL,LOW);
+  //digitalWrite(NSLEEP,LOW);
+  //digitalWrite(NRESET,HIGH);
+  //digitalWrite(MS3,LOW);
+  //digitalWrite(MS2,LOW);
+  //digitalWrite(MS1,LOW);
 }
 
 inline void setDir(bool d) {
   Mdir=d;
 }
 
-inline void setEna(bool e) {
-  digitalWrite(NENABLE,e);
-}
+/*inline void setEna(bool e) {
+  digitalWrite(DRIVER_EN,e);
+}*/
 
-inline void setSpeed(unsigned int s)
+inline void setSpeed(unsigned int s) // em relação a dt(pulsos).
 {
   NSpeed=s;
 }
@@ -80,8 +80,8 @@ inline void act(const unsigned long &t1)
       t0=t1;
     }
 
-    digitalWrite(DIR,Mdir);
-    digitalWrite(STEP,Mstep);
+    digitalWrite(DRIVER_DIR,Mdir);
+    digitalWrite(DRIVER_PUL,Mstep);
   }
    
 
--- Projeto Eletroeletrônico/Códigos Teste/Codigo 2 Ster Motor/controle.ino
+++ Projeto Eletroeletrônico/Códigos Teste/Codigo 2 Ster Motor/controle.ino
@@ -3,6 +3,39 @@ Daniel Gomes16:41
 #include "PComm.h"
 #include "StepMotor.h"
 
+#define POT_INSP_TIME A2         //tempo de inspiração
+#define POT_PIP A3               //Pressão inspiratória de pico
+#define POT_FREQUENCE A4         //frequência respiratória
+
+#define SENSOR_PRESSURE_1 A0
+
+// Limites dos controles
+const float TINSP_MIN = 0.3;
+const float TINSP_MAX = 6;
+const float PIP_MIN = 5;
+const float PIP_MAX = 55; 
+const float FREQ_MIN = 1;
+const float FREQ_MAX = 60;
+const float SENS_MIN = 0.4;
+const float SENS_MAX = 2;
+
+const float PIP_CONVERSOR = PIP_MIN + (PIP_MAX - PIP_MIN) / 1023;
+const float inspTime_CONVERSOR = TINSP_MIN + (TINSP_MAX - TINSP_MIN) / 1023;
+const float frequency_CONVERSOR = FREQ_MIN + (FREQ_MAX - FREQ_MIN) / 1023;
+
+const int VOLTAGE_CONVERSOR = 5/1023; // conversão do sinal digital para valores de tensão.
+
+const float PRESSURE_CONVERSOR = VOLTAGE_CONVERSOR * (120 + 10) / (4.5 - 0.28);
+
+float inspTime;             // Tempo (s) de inspiração
+float PIP;                  // Pressão inspiratória de pico (pressão máxima)
+float frequency;            // Frequência respiratória em rpm (respirações por minuto)
+unsigned long timer;
+int pulses = 3;             //pulsos por comando
+
+int fase;
+
+
 unsigned long t0;
 unsigned long t1;
 int interval=50;
@@ -13,6 +46,8 @@ void setup() {
 
   t0=micros();
   
+  
+    timer = actualTime();
 }
 
 
@@ -28,6 +63,57 @@ void loop() {
     //StepMotor::setEna(.)
   
   }
-  
-  StepMotor::act(t);
+
+
+void inputReader(){
+    PIP = PIP_CONVERSOR * analogRead(POT_PIP);
+    inspTime = inspTime_CONVERSOR * analogRead(POT_INSP_TIME);
+    frequency = frequency_CONVERSOR * analogRead(POT_FREQUENCE);
+}
+
+float pressureReader(){
+  float pressure = analogRead(SENSOR_PRESSURE_1) * PRESSURE_CONVERSOR;
+  return pressure;
+}
+
+void runControlLoop(){
+    switch(fase){
+        case 0: //inspiração
+            if (timer - actualTime() < inspTime){
+                    StepMotor::setDir(1); //direção de rotação
+                    StepMotor::setSpeed(1/frequency)        
+                    for(int i = 0; i < pulses; i++){ //pulsos ativos na borda de subida 
+                        if(pressureReader() < PIP){
+                          StepMotor::act(t);
+                          }
+                        }
+                    }
+                }
+            else{
+                timer = actualTime();
+                fase = 1;
+            }
+        break;
+
+        case 1: //expiração sem o peep        
+            if (timer - actualTime() < 1/frequency - inspTime) /*período - t_inspiração = t_expiração*/{
+                    StepMotor::setDir(0); //direção de rotação
+                    StepMotor::setSpeed(1/frequency)    //direção de rotação        
+                    for(int i = 0; i < pulses; i++){ //pulsos ativos na borda de subida 
+                        if(pressureReader() > 5){ //peep
+                          digitalWrite(DRIVER_PUL, LOW);
+                          digitalWrite(DRIVER_PUL, HIGH);
+                        }
+                    }
+                }        
+            else{
+            fase = 0;
+            timer = actualTime();            }
+
+        break;
+    }
+}
+
+unsigned long actualTime(){
+  return millis()*1e-3;
 }
